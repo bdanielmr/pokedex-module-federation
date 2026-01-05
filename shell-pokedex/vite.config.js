94a0bf5d5c16f28cc2da2274b3e4b93490ae2601
bdanielmr <bdanielmr@gmail.com>
2026-01-04 23:45:34 -0500

    feat: federation config

--- shell-pokedex/vite.config.js
+++ shell-pokedex/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "shell_pokedex",
+      remotes: {
+        pokemon_detail: "http://localhost:3001/assets/remoteEntry.js",
+        pokemon_history: "http://localhost:3002/assets/remoteEntry.js",
+      },
+      exposes: {
+        "./store": "./src/shared/store.js",
+      },
+      shared: ["react", "react-dom", "styled-components", "zustand", "react-router-dom"],
+    }),
+  ],
+  build: { target: "esnext" },
+  server: { port: 3000 },
+});
