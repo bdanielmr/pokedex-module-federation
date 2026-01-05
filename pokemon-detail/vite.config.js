efe228444a4f5a4f73e7021ae6cf9816fc070740
bdanielmr <bdanielmr@gmail.com>
2026-01-05 05:40:35 -0500

    feat: update remotes on microfrontend

--- pokemon-detail/vite.config.js
+++ pokemon-detail/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         "./PokemonDetailPage": "./src/PokemonDetailPage.jsx",
       },
       remotes: {
-        shell_pokedex: "http://localhost:3000/assets/remoteEntry.js",
+        shell_pokedex: "https://pokedex-module-federation.vercel.app/assets/remoteEntry.js",
       },
       shared: ["react", "react-dom", "styled-components", "zustand", "react-router-dom"],
     }),
